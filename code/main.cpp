1eea94de91392b9a919f079d20bf9bf124c2d0d4
Nick Kane <nkanedev@gmail.com>
2017-04-27 23:25:38 -0400

    adding SDL2 boilerplate startup code

---
+++ code/main.cpp
@@ -0,0 +1,55 @@
+#include <SDL.h>
+#include <stdio.h>
+
+#define global_variable static
+
+global_variable const int Screen_Width = 640;
+global_variable const int Screen_Height = 480;
+
+int
+main(int argc, char *argv[])
+{
+	// window to be rendered to
+	SDL_Window *window = NULL;
+
+	// surface contained by window
+	SDL_Surface *screenSurface = NULL; 
+
+	// Init SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("ERROR - SDL could not init - SDL_Error: %s\n", SDL_GetError());
+	}
+	else 
+	{
+		// create window
+		window = SDL_CreateWindow("Prototype Alpha 0.1", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, Screen_Width, Screen_Height, SDL_WINDOW_SHOWN);
+		if (window)
+		{
+			// get window surface
+			screenSurface = SDL_GetWindowSurface(window);
+
+			// fill surface white
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+			// update the surface
+			SDL_UpdateWindowSurface(window);
+
+			// wait five seconds
+			SDL_Delay(5000);
+		}
+		else
+		{
+			printf("ERROR - SDL could not create window - SDL_Error: %s\n", SDL_GetError());
+		}
+	}
+
+	// Destroy window
+	SDL_DestroyWindow(window);
+
+	// quit SDL subsystems
+	SDL_Quit();
+
+	printf("Hello ...\n");
+	return 0;
+}
