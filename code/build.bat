7a8afb951075d9960815c40206acc4eef66152e9
Nick Kane <nkanedev@gmail.com>
2017-04-29 00:14:09 -0400

    adding basic png file support, added sdl src files for debugger, and window can blit png files

--- code/build.bat
+++ code/build.bat
@@ -1,10 +1,11 @@
-IF NOT EXIST ..\build mkdir ..\build
-
+:: Go up one directory from code
 popd
+
+IF NOT EXIST .\build mkdir .\build
 
 pushd ..\build
 
-dir
+IF NOT EXIST .\assests mkdir .\assets
 
 :: TODO(nick): link SDL libs
 :: TODO(nick): create a SDL x86 / x64 build
@@ -13,6 +14,14 @@ dir
 cl -MTd -Od -nologo -Zi ..\code\main.cpp /I ..\code\include /link /LIBPATH:..\code\sdl2_x86\lib kernel32.lib legacy_stdio_definitions.lib winmm.lib gdi32.lib user32.lib mincore.lib shell32.lib version.lib imm32.lib sdl2.lib sdl2main.lib sdl2_image.lib /SUBSYSTEM:CONSOLE /NODEFAULTLIB:msvcrtd.lib
 
 copy ..\code\sdl2_x86\lib\SDL2.dll .\
+
+copy ..\code\sdl2_x86\lib\SDL2_Image.dll .\
+
+copy ..\code\sdl2_x86\lib\libpng16-16.dll .\
+
+copy ..\code\sdl2_x86\lib\zlib.dll .\
+
+copy ..\assets\* .\assets\
 
 popd
 
